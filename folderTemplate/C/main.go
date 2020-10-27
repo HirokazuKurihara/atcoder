7381dda7f0e02b674718e7e5df809749607df79b
kuriharahirokazu <hirokazu.kurihara@topgate.co.jp>
2020-10-27 22:32:33 +0900

    テンプレート作成

---
+++ folderTemplate/C/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+//TODO: テンプレートのためコピー後は修正or削除要
+func main() {
+	var a int
+	fmt.Scanf("%d", &a)
+	fmt.Print(ABCxxxC(a))
+}
+
+func ABCxxxC(a int) int {
+	return a
+}
---
+++ folderTemplate/F/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+//TODO: テンプレートのためコピー後は修正or削除要
+func main() {
+	var a int
+	fmt.Scanf("%d", &a)
+	fmt.Print(ABCxxxF(a))
+}
+
+func ABCxxxF(a int) int {
+	return a
+}
