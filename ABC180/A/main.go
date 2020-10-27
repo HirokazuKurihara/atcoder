b57d3bc557c98594719e7f087e63f19b6f37f032
kuriharahirokazu <hirokazu.kurihara@topgate.co.jp>
2020-10-27 22:26:34 +0900

    ABC180のA-Cの回答を追加

--- ABC180/A/main.go
+++ ABC180/A/main.go
@@ -1,1 +1,15 @@
-package A
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var n, a, b int
+	fmt.Scanf("%d %d %d", &n, &a, &b)
+	fmt.Print(ABC180A(n, a, b))
+}
+
+func ABC180A(n, a, b int) int {
+	return n - a + b
+}
--- ABC180/A/main_test.go
+++ ABC180/A/main_test.go
@@ -1,1 +1,39 @@
 package main
+
+import "testing"
+
+func TestABC180A(t *testing.T) {
+	type args struct {
+		n int
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "ok",
+			args: args{100, 1, 2},
+			want: 101,
+		},
+		{
+			name: "ok",
+			args: args{100, 2, 1},
+			want: 99,
+		},
+		{
+			name: "ok",
+			args: args{100, 1, 1},
+			want: 100,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ABC180A(tt.args.n, tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("ABC180A() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
