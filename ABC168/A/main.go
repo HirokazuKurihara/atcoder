420d607d70cb6d6ebf75702860683659938c5082
kuriharahirokazu <hirokazu.kurihara@topgate.co.jp>
2020-10-27 23:49:13 +0900

    ABC168のAを追加

---
+++ ABC168/A/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+// A - ∴ (Therefore)
+// https://atcoder.jp/contests/abc168/tasks/abc168_a
+func main() {
+	var n int
+	fmt.Scanf("%d", &n)
+
+	fmt.Println(ABC168A(n))
+}
+
+func ABC168A(n int) string {
+	n = n % 10
+
+	switch n {
+	//hon
+	case 2, 4, 5, 7, 9:
+		return "hon"
+	//pon
+	case 0, 1, 6, 8:
+		return "pon"
+	//bon
+	case 3:
+		return "bon"
+	default:
+		return ""
+	}
+}
