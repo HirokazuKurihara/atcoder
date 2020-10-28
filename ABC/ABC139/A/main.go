d060ba879506dbbca7be58df0e0d6642819a2739
kuriharahirokazu <hirokazu.kurihara@topgate.co.jp>
2020-10-28 22:36:25 +0900

    ABC139のAを追加

---
+++ ABC/ABC139/A/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+// A - Tenki
+// https://atcoder.jp/contests/abc139/tasks/abc139_a
+func main() {
+	var s, t string
+	fmt.Scanf("%s", &s)
+	fmt.Scanf("%s", &t)
+
+	fmt.Println(ABC139A(s, t))
+}
+
+func ABC139A(s, t string) int {
+
+	cnt := 0
+	for i := 0; i < 3; i++ {
+		if s[i] == t[i] {
+			cnt++
+		}
+	}
+
+	return cnt
+}
