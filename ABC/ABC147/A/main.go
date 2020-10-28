0b278128fcba50f521521b939c8ea3a325fae986
kuriharahirokazu <hirokazu.kurihara@topgate.co.jp>
2020-10-28 16:58:03 +0900

    ABC147のAを追加

---
+++ ABC/ABC147/A/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+// A - Blackjack
+// https://atcoder.jp/contests/abc147/tasks/abc147_a
+func main() {
+	var a, b, c int
+	fmt.Scanf("%d %d %d", &a, &b, &c)
+
+	fmt.Println(ABC147A(a, b, c))
+}
+
+func ABC147A(a, b, c int) string {
+	ret := "bust"
+	if a+b+c <= 21 {
+		ret = "win"
+	}
+
+	return ret
+}
