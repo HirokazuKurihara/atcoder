e81a059317e01bb8e461b9a00652339e9a377d46
kuriharahirokazu <hirokazu.kurihara@topgate.co.jp>
2020-10-29 22:39:00 +0900

    ABC176のCを追加

--- ABC/ABC176/C/main.go
+++ ABC/ABC176/C/main.go
@@ -2,29 +2,42 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
-// B - Multiple of 9
-// https://atcoder.jp/contests/abc176/tasks/abc176_b
+// C - Step
+// https://atcoder.jp/contests/abc176/tasks/abc176_c
 func main() {
-	var n string
-	fmt.Scanf("%s", &n)
+	n := ScanInt()
+	nums := ScanInts(n)
 
-	fmt.Print(ABC176B(n))
+	fmt.Print(ABC176C(nums))
 }
 
-func ABC176B(n string) string {
+func ABC176C(nums []int) int64 {
+	loopMax := len(nums)
 
-	sum := 0
-	for _, num := range n {
-		i, _ := strconv.Atoi(fmt.Sprintf("%c", num))
-		sum += i
+	var sum int64
+	for i := 0; i < loopMax-1; i++ {
+		if nums[i] > nums[i+1] {
+			diff := nums[i] - nums[i+1]
+			sum += int64(diff)
+			nums[i+1] += diff
+		}
 	}
 
-	if sum%9 == 0 {
-		return "Yes"
-	} else {
-		return "No"
+	return sum
+}
+
+func ScanInt() int {
+	var num int
+	fmt.Scanf("%d", &num)
+	return num
+}
+
+func ScanInts(n int) []int {
+	nums := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scanf("%d", &nums[i])
 	}
+	return nums
 }
--- ABC/ABC176/C/main_test.go
+++ ABC/ABC176/C/main_test.go
@@ -2,35 +2,50 @@ package main
 
 import "testing"
 
-func TestABC176B(t *testing.T) {
+func TestABC176C(t *testing.T) {
 	type args struct {
-		n string
+		nums []int
 	}
 	tests := []struct {
 		name string
 		args args
-		want string
+		want int
 	}{
 		{
-			name: "Yes",
-			args: args{"123456789"},
-			want: "Yes",
+			name: "ok",
+			args: args{[]int{2, 1, 5, 4, 3}},
+			want: 4,
 		},
 		{
-			name: "Yes",
-			args: args{"0"},
-			want: "Yes",
+			name: "ok",
+			args: args{[]int{3, 3, 3, 3, 3}},
+			want: 0,
 		},
 		{
-			name: "No",
-			args: args{"31415926535897932384626433832795028841971693993751058209749445923078164062862089986280"},
-			want: "No",
+			name: "ok",
+			args: args{[]int{3, 3, 3, 3, 3}},
+			want: 0,
+		},
+		{
+			name: "ok",
+			args: args{[]int{1, 2, 3, 4, 5}},
+			want: 0,
+		},
+		{
+			name: "ok",
+			args: args{[]int{5, 4, 3, 2, 1}},
+			want: 10,
+		},
+		{
+			name: "ok",
+			args: args{[]int{1, 5, 3, 2, 4}},
+			want: 6,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := ABC176B(tt.args.n); got != tt.want {
-				t.Errorf("ABC176B() = %v, want %v", got, tt.want)
+			if got := ABC176C(tt.args.nums); got != tt.want {
+				t.Errorf("ABC176C() = %v, want %v", got, tt.want)
 			}
 		})
 	}
