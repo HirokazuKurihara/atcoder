02dc1af5acc0d45ea4a93a6a6a2d852de1efb43d
kuriharahirokazu <hirokazu.kurihara@topgate.co.jp>
2020-10-29 22:41:59 +0900

    scanのパターン追加

--- util/scan.go
+++ util/scan.go
@@ -15,3 +15,31 @@ func ScanInts(n int) []int {
 	}
 	return nums
 }
+
+func ScanFloat() float64 {
+	var num float64
+	fmt.Scanf("%f", &num)
+	return num
+}
+
+func ScanFloats(n int) []float64 {
+	nums := make([]float64, n)
+	for i := 0; i < n; i++ {
+		fmt.Scanf("%f", &nums[i])
+	}
+	return nums
+}
+
+func ScanString() string {
+	var str string
+	fmt.Scanf("%s", &str)
+	return str
+}
+
+func ScanStrings(n int) []string {
+	strs := make([]string, n)
+	for i := 0; i < n; i++ {
+		fmt.Scanf("%s", &strs[i])
+	}
+	return strs
+}
